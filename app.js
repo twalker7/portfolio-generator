76289c9f6f49e5cafc986b084133f9f7079bdd07
Thomas Walker <thomasnwalker@yahoo.com>
2021-06-11 04:56:26 -0700

    good night/morning commit

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+var fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2); 
+
+// destructor time for name and github variables!!!!!
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), (err)=>{
+   if(err) throw err;
+
+   console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+
+/* reference of input console function from lesson 9.1
 const profileDataArgs = process.argv.slice(2, process.argv.length); 
-console.log(profileDataArgs);
+
 
 const printProfileData = (profileDataArr)=>{
    profileDataArr.forEach(item=> console.log(item));
 };
 
 printProfileData(profileDataArgs);
+*/ 
